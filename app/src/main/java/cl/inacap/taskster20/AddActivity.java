6d4d26a44350435ef1100dc3e93bb9b192f7b52f
GastonUC <Gacox.magallanes@gmail.com>
2020-12-20 22:11:11 -0300

    Agrega calendario funcionando, pero sin realizar ingreso a la BDD

--- app/src/main/java/cl/inacap/taskster20/AddActivity.java
+++ app/src/main/java/cl/inacap/taskster20/AddActivity.java
@@ -3,6 +3,7 @@ package cl.inacap.taskster20;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,8 +11,10 @@ import android.view.View;
 import android.view.animation.CycleInterpolator;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class AddActivity extends AppCompatActivity {
@@ -19,6 +22,7 @@ public class AddActivity extends AppCompatActivity {
     private Context context;
     Activity activity;
     EditText titleNota, descripNota;
+    TextView text_date;
     Button add_button;
     ImageButton date_button;
 
@@ -29,6 +33,7 @@ public class AddActivity extends AppCompatActivity {
 
         titleNota = findViewById(R.id.titleNota);
         descripNota = findViewById(R.id.descripNota);
+        text_date = findViewById(R.id.text_date);
         add_button = findViewById(R.id.add_button);
         add_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,12 +53,28 @@ public class AddActivity extends AppCompatActivity {
         });
         date_button = findViewById(R.id.date_button);
         date_button.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.date_button:
+                        mostrarCalendario();
+                        break;
+                }
                 Toast.makeText(AddActivity.this, "Se ha presionado botón Calendario", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void mostrarCalendario() {
+        //Primero se hace DatePickerFrag,emt
+        DatePickerFragment newFragment = DatePickerFragment.instancia(new DatePickerDialog.OnDateSetListener() {
+           @Override
+           public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+               final String date = day + "/" + (month+1) + "/" + year;
+               text_date.setText(date);
+           }
+        });
+        newFragment.show(getSupportFragmentManager(),"datepicker");
     }
 
     //Método para animar la validación de los EditText
